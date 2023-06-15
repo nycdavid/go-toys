1b2ce32a7d363dc80841b0497794ac224e5a7e17
David Ko <davidsj.ko@gmail.com>
2023-06-15 10:11:03 -0400

    Create struct to house api resp

--- weathercli/cmd/root.go
+++ weathercli/cmd/root.go
@@ -4,6 +4,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,6 +13,22 @@ import (
 
 	"github.com/spf13/cobra"
 )
+
+type Response struct {
+	Info    Info    `json:"current"`
+	Request Request `json:"request"`
+}
+
+type Info struct {
+	Temperature         int      `json:"temperature"`
+	Humidity            int      `json:"humidity"`
+	FeelsLike           int      `json:"feelslike"`
+	WeatherDescriptions []string `json:"weather_descriptions"`
+}
+
+type Request struct {
+	Query string `json:"query"`
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -36,13 +53,23 @@ var rootCmd = &cobra.Command{
 
 		resp, e := http.Get(url)
 		if e != nil {
-			log.Println(e)
+			log.Fatal(e)
 		}
 		defer resp.Body.Close()
 
 		body, e := io.ReadAll(resp.Body)
+		if e != nil {
+			log.Fatal(e)
+		}
 
-		fmt.Println(string(body))
+		var data Response
+		e = json.Unmarshal(body, &data)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		fmt.Println(data)
+		fmt.Printf("%+v\n", data)
 	},
 }
 
